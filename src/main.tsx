3326f0fd332ab1299119284672d2dbe3e49f1bab
habysabdo <73203563+habysabdo@users.noreply.github.com>
2026-07-30 23:06:33 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './polyfills';
+// import './polyfills'; <-- Comment this out for now to test the web build
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
