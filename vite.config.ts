c5e2ef7c801d34cadb7cddc37e70acc186195779
habysabdo <73203563+habysabdo@users.noreply.github.com>
2026-07-30 22:43:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- ADD THIS LINE HERE
   plugins: [
     react(),
     legacy({
