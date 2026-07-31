2ac10545da483140c603374ae35d41136a36ec27
habysabdo <73203563+habysabdo@users.noreply.github.com>
2026-07-31 00:03:54 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  // Add any other custom VITE_ variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
