1d69b531c58580b72c674ca3eaeb5cc2a4952995
unknown <hany@example.com>
2026-08-01 20:32:57 -0700

    Remove legacy plugin to fix polyfill MIME type error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
@@ -8,15 +7,9 @@ export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    legacy({
-      targets: ['Chrome >= 70', 'Android >= 7'],
-      modernPolyfills: true,
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
   ],
   build: {
     outDir: 'dist',
-    target: ['es2015', 'chrome70'],
   },
   resolve: {
     alias: {
